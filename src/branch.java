45e1db40ca2a002769cacf7a1a3a5e8736a87fcb
kians376 <kianshah376@gmail.com>
2019-11-16 17:23:39 -0800

    Final gitignore fix commit

---
+++ src/branch.java
@@ -0,0 +1,213 @@
+
+// We need to import the java.sql package to use JDBC
+import java.sql.*;
+
+// for reading from the command line
+import java.io.*;
+
+// for the login window
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+/*
+ * This class implements a graphical login window and a simple text
+ * interface for interacting with the branch table
+ */
+public class branch implements ActionListener
+{
+    // command line reader
+    private BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+    private Connection con;
+
+    // user is allowed 3 login attempts
+    private int loginAttempts = 0;
+
+    // components of the login window
+    private String usernameField = "ora_ldobrien";
+    private String passwordField = "a34891416";
+    private JFrame mainFrame;
+
+
+    /*
+     * constructs login window and loads JDBC driver
+     */
+    public branch()
+    {
+        mainFrame = new JFrame("User Login");
+
+        JButton loginButton = new JButton("Log In");
+
+        JPanel contentPane = new JPanel();
+        mainFrame.setContentPane(contentPane);
+
+
+        // layout components using the GridBag layout manager
+
+        GridBagLayout gb = new GridBagLayout();
+        GridBagConstraints c = new GridBagConstraints();
+
+        contentPane.setLayout(gb);
+        contentPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        // place the login button
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        c.insets = new Insets(5, 10, 10, 10);
+        c.anchor = GridBagConstraints.CENTER;
+        gb.setConstraints(loginButton, c);
+        contentPane.add(loginButton);
+
+        // OK button with action event handler
+        loginButton.addActionListener(this);
+
+        // anonymous inner class for closing the window
+        mainFrame.addWindowListener(new WindowAdapter()
+        {
+            public void windowClosing(WindowEvent e)
+            {
+                System.exit(0);
+            }
+        });
+
+        // size the window to obtain a best fit for the components
+        mainFrame.pack();
+
+        // center the frame
+        Dimension d = mainFrame.getToolkit().getScreenSize();
+        Rectangle r = mainFrame.getBounds();
+        mainFrame.setLocation( (d.width - r.width)/2, (d.height - r.height)/2 );
+
+        // make the window visible
+        mainFrame.setVisible(true);
+
+        try
+        {
+            // Load the Oracle JDBC driver
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        }
+        catch (SQLException ex)
+        {
+            System.out.println("Message: " + ex.getMessage());
+            System.exit(-1);
+        }
+    }
+
+
+    /*
+     * connects to Oracle database named ug using user supplied username and password
+     */
+    private boolean connect()
+    {
+        String connectURL = "jdbc:oracle:thin:@localhost:1522:stu";
+
+        try
+        {
+            con = DriverManager.getConnection(connectURL,usernameField,passwordField);
+
+            System.out.println("\nConnected to Oracle!");
+            return true;
+        }
+        catch (SQLException ex)
+        {
+            System.out.println("Message: " + ex.getMessage());
+            return false;
+        }
+    }
+
+
+    /*
+     * event handler for login window
+     */
+    public void actionPerformed(ActionEvent e)
+    {
+        if ( connect() )
+        {
+            // if the username and password are valid,
+            // remove the login window and display a text menu
+            mainFrame.dispose();
+            showMenu(con);
+        }
+        else
+        {
+            loginAttempts++;
+
+            if (loginAttempts >= 3)
+            {
+                mainFrame.dispose();
+                System.out.println("Login Attempt Failed");
+                System.exit(-1);
+            }
+        }
+
+    }
+
+
+    /*
+     * displays simple text interface
+     */
+    public void showMenu(Connection con)
+    {
+        int choice;
+        boolean quit;
+        customer c = new customer();
+        int currentChoice = 0;
+        quit = false;
+
+        try
+        {
+            // disable auto commit mode
+            con.setAutoCommit(false);
+
+            while (!quit)
+            {
+                System.out.print("\n\nPlease choose one of the following: \n");
+
+                if(currentChoice == 1) {
+                    c.customerMenu(con);
+                } else {
+                    System.out.print("1:  Customer Menu: \n");
+                    System.out.print("2:  Reports \n");
+                    System.out.print("3:  Clerk \n");
+                    System.out.print("5.  Quit\n>> ");
+                }
+
+                choice = Integer.parseInt(in.readLine());
+
+                System.out.println(" ");
+
+                switch(choice)
+                {
+                    case 1:  currentChoice = 1; break;
+//                    case 2:  deleteBranch(); break;
+//                    case 3:  updateBranch(); break;
+//                    case 4:  showBranch(); break;
+                    case 5:  quit = true;
+                }
+            }
+
+            con.close();
+            in.close();
+            System.out.println("\nGood Bye!\n\n");
+            System.exit(0);
+        }
+        catch (IOException e)
+        {
+            System.out.println("IOException!");
+
+            try
+            {
+                con.close();
+                System.exit(-1);
+            }
+            catch (SQLException ex)
+            {
+                System.out.println("Message: " + ex.getMessage());
+            }
+        }
+        catch (SQLException ex) {
+            System.out.println("Message: " + ex.getMessage());
+        }
+    }
+}
