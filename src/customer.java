0348577cd46e9240b48b7930f39e4bae48da8e48
michu <56609665+michutrain@users.noreply.github.com>
2019-11-18 01:59:00 -0800

    implemented customer

--- src/customer.java
+++ src/customer.java
@@ -3,51 +3,150 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.*;
 
-public class customer {
+public class Customer {
 
-    public void addVehicle() {
-//        try {
-//            ps = con.prepareStatement(
-//            "INSERT INTO VEHICLE(vid, vlicense, make, model, year, color, odometer, status, vtname, location, city)" +
-//                    "VALUES (1234, '4321', 'Ford', 'Mustang', '2019', 'Red', 0, 'True', 'car', 'Kits', 'Vancouver')");
-//            ps.executeUpdate();
-//            System.out.println("Car Added\n");
-//        } catch (SQLException ex) {
-//            System.out.println("Message: " + ex.getMessage());
-//        }
+    private PreparedStatement addVehicle;
+    private PreparedStatement addCustomer;
+    private PreparedStatement makeReservation;
+    private PreparedStatement getAvailableVehicle;
+    private PreparedStatement getAvailableVehiclesDetails;
+    private PreparedStatement getVehicleStatement;
+    private PreparedStatement getCustomerByPhoneNum;
+    private PreparedStatement getAvailableVehiclesCount;
+
+    private MainMenu mainMenu;
+
+    private final String addVehicleQuery =
+        "INSERT INTO Vehicle (vid, vlicense, make, model," +
+        " year, color, odometer, status, vtname, location, city)" +
+        " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+    
+    private final String addCustomerQuery =
+        "INSERT INTO Customer" +
+        " (dlicense, name, phoneNum, address)" +
+        " VALUES (?, ?, ?, ?)";
+
+    private final String makeReservationQuery =
+        "INSERT INTO Reservation" +
+        " (vtname, dlincense, fromTimestamp, toTimestamp)" +
+        " VALUES (?, ?, ?, ?)";
+                
+    private final String getAvailableVehicleQuery =
+        "SELECT vid FROM Vehicle WHERE vtname = ? limit 1";
+
+    private final String getVehicleQuery =
+        "UPDATE Vehicle SET status = ? WHERE vid = ?";
+
+    private final String getCustomerByPhoneNumQuery =
+        "SELECT count(*) FROM Customer WHERE phoneNum = ?";
+
+    private final String availableVehiclesDetailsQuery = 
+        "SELECT * FROM VEHICLE WHERE vtname = ? and location = ?";
+
+    private final String getAvailableVehiclesCountQuery =
+        "SELECT count(*) FROM Vehicle WHERE vtname = ? and location = ?";
+
+    public Customer(MainMenu mainMenu) {
+        this.mainMenu = mainMenu;
+        
+        try {
+            addVehicle = this.mainMenu.con.prepareStatement(addVehicleQuery);
+
+            addCustomer = this.mainMenu.con.prepareStatement(addCustomerQuery); 
+
+            makeReservation = this.mainMenu.con.prepareStatement(makeReservationQuery);
+
+            getAvailableVehicle = this.mainMenu.con.prepareStatement(getAvailableVehicleQuery);
+            
+            getVehicleStatement = this.mainMenu.con.prepareStatement(getVehicleQuery);
+
+            getCustomerByPhoneNum = this.mainMenu.con.prepareStatement(getCustomerByPhoneNumQuery);
+
+            getAvailableVehiclesDetails = this.mainMenu.con.prepareStatement(availableVehiclesDetailsQuery);
+
+            getAvailableVehiclesCount = this.mainMenu.con.prepareStatement(getAvailableVehiclesCountQuery);
+        } catch (SQLException ex) {
+            System.out.println("Message: " + ex.getMessage());
+        }
     }
 
-    public void addCustomer(String name, String phoneNum){
-        // TODO: Add customer to database
+    void addVehicle(long vid, long vlicense, String make, String model, long year, String color, long odometer,
+                    String status, String vtname, String location, String city) throws SQLException {
+        addVehicle.setLong(0, vid);
+        addVehicle.setLong(1, vlicense);
+        addVehicle.setString(2, make);
+        addVehicle.setString(3, model);
+        addVehicle.setLong(4, year);
+        addVehicle.setString(5, color);
+        addVehicle.setLong(6, odometer);
+        addVehicle.setString(7, status);
+        addVehicle.setString(8, vtname);
+        addVehicle.setString(9, location);
+        addVehicle.setString(10, city);
+
+        addVehicle.executeUpdate();
     }
 
-    public int makeReservation(String carType, String location, String fromDay, String fromTime, String toDay, String toTime){
+    public void addCustomer(String dlicense, String name, long phoneNum, String address) throws SQLException {
+        addCustomer.setString(0, dlicense);
+        addCustomer.setString(1, name);
+        addCustomer.setLong(2, phoneNum);
+        addCustomer.setString(3, address);
+
+        addVehicle.executeUpdate();
+    }
+
+    private void setVehicleStatus(long vid, int status) throws SQLException { // 0: available, 1: rented, 2: maintainence
+        getVehicleStatement.setInt(0, status);
+        getVehicleStatement.setLong(1, vid);
+        getVehicleStatement.executeQuery();
+    }
+
+        
+    public int makeReservation(String vtname, String dlincense,
+        String fromTimestamp, String toTimestamp) throws SQLException{
         // TODO: Make a reservation for the parameters
         // TODO: Set the vehicle Status to reserved
         // TODO: return the confirmation number (reservation CONFNO)
-        return 0; // stub
+        
+        getAvailableVehicle.setString(0, vtname);
+        ResultSet result = getAvailableVehicle.executeQuery();
+        if (result.next()) {
+            setVehicleStatus(result.getInt("vid"), 1);
+            makeReservation.setString(0, vtname);
+            makeReservation.setString(1, dlincense);
+            makeReservation.setString(2, fromTimestamp);
+            makeReservation.setString(3, toTimestamp);
+            
+            return makeReservation.executeUpdate();
+        }
+
+        return 0; //incase no exist
+        
     }
 
-    public boolean validCustomer(String name, String phoneNum){
-        // TODO: Check is a customer is a valid customer or not
-        return false;
+    public boolean validCustomer(String phoneNum) throws SQLException {
+        getCustomerByPhoneNum.setString(0, phoneNum);
+        ResultSet results = getCustomerByPhoneNum.executeQuery();
+        return results.getInt("total") > 0;
     }
 
-    public int getAvailableVehiclesCount(String carType, String location, String fromDay, String fromTime, String toDay, String toTime) {
-        // TODO: Get the number of available vehicles matching the inputs (inputs could be null) and return it as a string
-        return 0; // Stub
+    public int getAvailableVehiclesCount(String carType, String location) throws SQLException {
+        getAvailableVehiclesCount.setString(0, carType == null ? "*" : carType);
+        getAvailableVehiclesCount.setString(1, location == null ? "*" : location);
+        ResultSet results = getAvailableVehiclesCount.executeQuery();
+        return results.getInt("total");
     }
 
-    public void showAvailableVehiclesDetails(String carType, String location, String fromDay, String fromTime, String toDay, String toTime) {
-        Statement stmt;
+    public void showAvailableVehiclesDetails(String carType, String location) throws SQLException {
         ResultSet rs;
 
-        try
-        {
+        try {
             // TODO: This needs to work if any/all of the above params are empty
             // TODO: If all params are empty, returns all available vehicles at that branch
-            stmt = MainMenu.con.createStatement();
-            rs = stmt.executeQuery("SELECT * FROM VEHICLE"); // TODO: Needs to have a condition
+            getAvailableVehiclesDetails.setString(0, carType == null ? "*" : carType);
+            getAvailableVehiclesDetails.setString(1, location == null ? "*" : location);
+            rs = getAvailableVehiclesDetails.executeQuery();
 
             // get info on ResultSet
             ResultSetMetaData rsmd = rs.getMetaData();
@@ -55,17 +154,15 @@ public class customer {
             // get number of columns
             int numCols = rsmd.getColumnCount() > 15 ? 15 : rsmd.getColumnCount();
 
-            System.out.println(" ");
+            System.out.println();
 
             // display column names;
-            for (int i = 0; i < numCols; i++)
-            {
+            for (int i = 0; i < numCols; i++) {
                 // get column name and print it
-                System.out.printf("%-15s", rsmd.getColumnName(i+1));
+                System.out.printf("%-15s", rsmd.getColumnName(i + 1));
             }
             int count = 0;
-            while(rs.next() && count < 15)
-            {
+            while (rs.next() && count < 15) {
                 String make = rs.getString("MAKE");
                 System.out.printf("%-10.10s", make);
 
@@ -76,13 +173,7 @@ public class customer {
                 System.out.printf("%-20.20s", vLocation);
                 count++;
             }
-
-            // close the statement;
-            // the ResultSet will also be closed
-            stmt.close();
-        }
-        catch (SQLException ex)
-        {
+        } catch (SQLException ex) {
             System.out.println("Message: " + ex.getMessage());
         }
     }
