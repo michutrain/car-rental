cf0ccf0a2b3994c2a9e6c1921dc199e4b8a20017
michu <56609665+michutrain@users.noreply.github.com>
2019-11-24 04:06:00 -0800

    updated methods

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,5 @@
 import Util.TimeInterval;
+
 import java.sql.*;
 
 
@@ -17,61 +18,61 @@ public class Customer {
     private MainMenu mainMenu;
 
     private final String addVehicleQuery =
-        "INSERT INTO Vehicle (vid, vlicense, make, model," +
-        " year, color, odometer, status, vtname, location, city)" +
-        " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
-    
+            "INSERT INTO Vehicle (vid, vlicense, make, model," +
+                    " year, color, odometer, status, vtname, location, city)" +
+                    " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+
     private final String addCustomerQuery =
-        "INSERT INTO Customer" +
-        " (dlicense, name, phoneNum, address)" +
-        " VALUES (?, ?, ?, ?)";
+            "INSERT INTO Customer" +
+                    " (dlicense, name, phoneNum, address)" +
+                    " VALUES (?, ?, ?, ?)";
 
     private final String makeReservationQuery =
-        "INSERT INTO Reservation" +
-        " (vtname, dlincense, fromTimestamp, toTimestamp)" +
-        " VALUES (?, ?, ?, ?)";
-                
+            "INSERT INTO Reservation" +
+                    " (vtname, dlincense, fromTimestamp, toTimestamp)" +
+                    " VALUES (?, ?, ?, ?)";
+
     private final String getAvailableVehicleQuery =
-        "SELECT vid FROM Vehicle WHERE vtname = ? limit 1";
+            "SELECT vid FROM Vehicle WHERE vtname = ? limit 1";
 
     private final String getVehicleQuery =
-        "UPDATE Vehicle SET status = ? WHERE vid = ?";
+            "UPDATE Vehicle SET status = ? WHERE vid = ?";
 
     private final String getCustomerByPhoneNumQuery =
-        "SELECT COUNT(*) FROM Customer WHERE phoneNum = ?";
+            "SELECT COUNT(*) FROM Customer WHERE phoneNum = ?";
 
-    private final String availableVehiclesDetailsQuery = 
-        "SELECT * FROM VEHICLE WHERE vtname = ? and location = ?";
+    private final String getAvailableVehiclesDetailsQuery =
+            "SELECT * FROM VEHICLE WHERE vtname = ? and location = ?";
 
     private final String getAvailableVehiclesCountQuery =
-        "SELECT * FROM Vehicle";
-    private final String getAvailableVehiclesDetailsQuery =
-        "SELECT * FROM Vehicle";
+            "SELECT * FROM Vehicle";
+
 
     private final String getCustomerInformationQuery =
             "SELECT * FROM CUSTOMER WHERE name = ? AND CELLPHONE = ?";
 
     public Customer(MainMenu mainMenu) {
         this.mainMenu = mainMenu;
-        
+
         try {
             addVehicle = this.mainMenu.con.prepareStatement(addVehicleQuery);
 
-            addCustomer = this.mainMenu.con.prepareStatement(addCustomerQuery); 
+            addCustomer = this.mainMenu.con.prepareStatement(addCustomerQuery);
 
             makeReservation = this.mainMenu.con.prepareStatement(makeReservationQuery);
 
             getAvailableVehicle = this.mainMenu.con.prepareStatement(getAvailableVehicleQuery);
-            
+
             getVehicleStatement = this.mainMenu.con.prepareStatement(getVehicleQuery);
 
             getCustomerByPhoneNum = this.mainMenu.con.prepareStatement(getCustomerByPhoneNumQuery);
 
-            getAvailableVehiclesDetails = this.mainMenu.con.prepareStatement(availableVehiclesDetailsQuery);
+            getAvailableVehiclesDetails = this.mainMenu.con.prepareStatement(getAvailableVehiclesDetailsQuery);
 
             getAvailableVehiclesCount = this.mainMenu.con.prepareStatement(getAvailableVehiclesCountQuery);
 
             getCustomerInformation = this.mainMenu.con.prepareStatement(getCustomerInformationQuery);
+
         } catch (SQLException ex) {
             System.out.println("Message: " + ex.getMessage());
         }
@@ -104,9 +105,21 @@ public class Customer {
     }
 
     public ResultSet getCustomerInformation(String name, long phoneNum) throws SQLException {
-        getCustomerInformation.setString(1, name == null ? "*" : name);
-        getCustomerInformation.setString(2, phoneNum == 0 ? "*" : Long.toString(phoneNum));
-        return getAvailableVehiclesDetails.executeQuery();
+        Statement stmt = mainMenu.con.createStatement();
+        String sqlStatement = getAvailableVehiclesCountQuery;
+
+        if (name != null) {
+            sqlStatement += " WHERE name = " + name;
+
+            if (phoneNum != 0) {
+                sqlStatement += " AND phoneNum = " + phoneNum;
+            }
+
+        } else if (phoneNum != 0) {
+            sqlStatement += "WHERE phoneNum = " + phoneNum;
+        }
+
+        return stmt.executeQuery(sqlStatement);
     }
 
     private void setVehicleStatus(long vid, int status) throws SQLException { // 0: available, 1: rented, 2: maintainence
@@ -115,8 +128,7 @@ public class Customer {
         getVehicleStatement.executeQuery();
     }
 
-        
-    public int makeReservation(String vtname, String dlincense, TimeInterval interval) throws SQLException{
+    public int makeReservation(String vtname, String dlincense, TimeInterval interval) throws SQLException {
         getAvailableVehicle.setString(1, vtname);
         ResultSet result = getAvailableVehicle.executeQuery();
         if (result.next()) {
@@ -125,12 +137,11 @@ public class Customer {
             makeReservation.setString(2, dlincense);
             makeReservation.setString(3, interval.getFrom().toString());
             makeReservation.setString(4, interval.getTo().toString());
-            
+
             return makeReservation.executeUpdate();
         }
 
         return 0; //incase no exist
-        
     }
 
     public boolean validCustomer(String phoneNum) throws SQLException {
@@ -141,30 +152,28 @@ public class Customer {
 
     public int getAvailableVehiclesCount(String carType, String location, TimeInterval interval) throws SQLException {
         Statement stmt = mainMenu.con.createStatement();
-
         String sqlStatement = getAvailableVehiclesCountQuery;
 
         if (carType != null) {
             sqlStatement += " WHERE vtname = " + carType;
 
             if (location != null) {
-            sqlStatement += " AND location = " + location;
+                sqlStatement += " AND location = " + location;
             }
 
             if (interval != null) {
-             sqlStatement += " AND status = 0 ";
+                sqlStatement += " AND status = 0 ";
             }
-        }
 
-        else if (location != null) {
+        } else if (location != null) {
             sqlStatement += "WHERE location = " + location;
 
             if (interval != null) {
-            sqlStatement += " AND status = 0 ";
+                sqlStatement += " AND status = 0 ";
             }
-        }
-        else if (interval != null) {
-            sqlStatement += " AND status = 0 ";
+
+        } else if (interval != null) {
+            sqlStatement += "WHERE status = 0 ";
         }
 
         sqlStatement += " ORDER BY location, vtname";
@@ -174,9 +183,21 @@ public class Customer {
     }
 
     public ResultSet getAvailableVehicles(String carType, String location) throws SQLException {
-        getAvailableVehiclesDetails.setString(1, carType == null ? "*" : carType);
-        getAvailableVehiclesDetails.setString(2, location == null ? "*" : location);
-        return getAvailableVehiclesDetails.executeQuery();
+        Statement stmt = mainMenu.con.createStatement();
+        String sqlStatement = getAvailableVehiclesCountQuery;
+
+        if (carType != null) {
+            sqlStatement += " WHERE vtname = " + carType;
+
+            if (location != null) {
+                sqlStatement += " AND location = " + location;
+            }
+
+        } else if (location != null) {
+            sqlStatement += "WHERE location = " + location;
+        }
+
+        return stmt.executeQuery(sqlStatement);
     }
 
     public void showAvailableVehiclesDetails(String carType, String location) throws SQLException {
